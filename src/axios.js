465c9853b0a70cdc4a0c276b53df44cc6d27b59b
marcstampfli <marcstampfli@gmail.com>
2020-09-15 07:03:42 -0400

    Updates

---
+++ src/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const instance = axios.create({
+  //baseURL: "http://localhost:8001",
+  baseURL: "https://mern-tinder-backend.herokuapp.com",
+});
+
+export default instance;
+
